5945ea9d4b6bbcaa3025998f4ded573949eb5a13
Pronob <pronobmozumder.info@gmail.com>
2021-02-11 03:49:01 +0600

    functional and class based components working

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Myapp from './componantes/compo1';
+import Myapp2 from './componantes/compo2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> 
+  <Myapp2 />
+  <Myapp />
+  </div>
   );
 }
 
