8db4100b2b6c56c321bb30286e61eff311e8904d
Pronob <pronobmozumder.info@gmail.com>
2021-02-11 05:14:00 +0600

    error checked

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Myapp from './components/compo1'
+import Book from './components/book';
+
 
 function App() {
   return (
     <div> 
   <h1>Hello, js.</h1>
   <Myapp />
+  <Book />
   </div>
   );
 }
--- testapp/src/components/compo1.js
+++ testapp/src/components/compo1.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Myapp(){
     return(
+        <div>
         <h1>Hello, World!!</h1>
+        </div>
     );
 }
 
